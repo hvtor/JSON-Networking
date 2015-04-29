1b5df5f5f763c29469923468ff09404187709bd4
Hemant Torsekar <hvt@me.com>
2015-04-29 16:15:58 -0400

    Bitly URL shortener API - JSON Networking Lecture

--- Shawteh/ViewController.m
+++ Shawteh/ViewController.m
@@ -14,9 +14,34 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _APILogin = @"o_1s5r2bdu1p";
+    _APIKey = @"R_71dc764e426a4e2a96c4e3c194c47b6a";
+    _longURL = @"http://rypress.com/tutorials/objective-c/index";
+    _requestString = [NSString stringWithFormat:@"http://api.bit.ly/shorten?version=2.0.1&longUrl=%@&login=%@&apiKey=%@&format=json",_longURL, _APILogin, _APIKey];
+    
+    NSURL *requestURL = [NSURL URLWithString:_requestString];
+    NSURLSession *session = [NSURLSession sharedSession];
+    
+    [[session dataTaskWithURL:requestURL
+            completionHandler:^(NSData *data, NSURLResponse *response,
+            NSError *error){
+                //handle errors
+                NSError *e = nil;
+                NSDictionary *bitlyJSON = [NSJSONSerialization JSONObjectWithData:data options:0 error:&e];
+                NSLog(@"%@", bitlyJSON);
+                if (!error) {
+                    NSDictionary *results = [bitlyJSON objectForKey:@"results"];
+                    NSDictionary *resultsForLongURL = [results objectForKey:_longURL];
+                    NSString *shortURL = [resultsForLongURL objectForKey:@"shortUrl"];
+                    NSLog(@"The short URL = %@", shortURL);
+                }
+                // Parse JSON
+                // return JSON value
+            }]resume];
+        sleep(60);
 }
 
 - (void)didReceiveMemoryWarning {
